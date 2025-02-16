8f5f130445e78cb5df1e93f1e1961e59652e7c94
aiym123 <ayymzhan.kazbekova.06@mail.ru>
2025-02-16 20:37:40 +0500

    init commit

---
+++ src/main/java/org/example/githubbackend/GithubbackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.githubbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubbackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/githubbackend/GithubbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.githubbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubbackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
