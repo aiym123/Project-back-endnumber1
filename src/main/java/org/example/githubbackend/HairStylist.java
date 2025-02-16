72598a5e8f22fd7adac55f5397ef3fa3c685f590
aiym123 <ayymzhan.kazbekova.06@mail.ru>
2025-02-16 21:09:32 +0500

    Finalize and test application

---
+++ src/main/java/org/example/githubbackend/HairStylist.java
@@ -0,0 +1,10 @@
+package org.example.githubbackend;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class HairStylist {
+    public String getStylistName() {
+        return "Aiymzhan Kazbekova the best stylist";
+    }
+}
