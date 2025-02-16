1149a28f613a7fb6e9b8cf55fbb14f5e9e8ade12
aiym123 <ayymzhan.kazbekova.06@mail.ru>
2025-02-16 21:09:14 +0500

    Add first bean implementation

---
+++ src/main/java/org/example/githubbackend/BeautySalon.java
@@ -0,0 +1,10 @@
+package org.example.githubbackend;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class BeautySalon {
+    public String getSalonName() {
+        return "Glamour Beauty Salon";
+    }
+}
